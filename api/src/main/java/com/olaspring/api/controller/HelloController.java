a8b348528996fbdca765f4c5b2be973ac59da75a
guilhermeicarofr <guilhermeicarofr@gmail.com>
2023-03-27 19:21:37 -0300

    spring: intializing project with spring framework

---
+++ api/src/main/java/com/olaspring/api/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.olaspring.api.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+  @GetMapping
+  public String hello() {
+    return("Hello from Spring Framework!");
+  }
+}
